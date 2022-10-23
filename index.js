4c3ef0c4d28b9874dd2363a03733f43b9330cb2a
MakHurst <makhurst.tech@gmail.com>
2022-10-22 20:26:10 -0700

    code updates

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ document.addEventListener("DOMContentLoaded", () =>
     const result = document.querySelector("#result")
     const displayCurrentPlayer = document.querySelector("#current-player")
     let currentPlayer = 1  // used let because it will not always be player 1's turn
+    function checkBoard(){
+        
+
+    }
 
     for (let i = 0; i < squares.length; i++) {
         squares[i].onclick = () => {
@@ -11,12 +15,20 @@ document.addEventListener("DOMContentLoaded", () =>
             // if the square below your current square is taken, you can go above it
             if (squares[i + 7].classList.contains('taken')) {
                 if (currentPlayer == 1){
-                    squares[1].classList.add('taken')
-                    squares[1].classList.add('player-one')
+                    squares[i].classList.add('taken')
+                    squares[i].classList.add('player-one')
                     currentPlayer = 2
                     displayCurrentPlayer.innerHTML = currentPlayer
 
                 }
+                else if (currentPlayer == 2){
+                    squares[i].classList.add('taken')
+                    squares[i].classList.add('player-two')
+                    currentPlayer = 1
+                    displayCurrentPlayer.innerHTML = currentPlayer
+
+                }
+            else alert("You cannot chose that box")
             }
         
         }
